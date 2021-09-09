40f1e10c2d44000eaf0e242befc74b1932029824
souheibmadi <sm@aimngt.com>
2021-09-09 11:16:43 +0100

    quantity hubs new

--- routes/api.php
+++ routes/api.php
@@ -33,7 +33,7 @@ Route::delete('/deletewilaya/{id}', 'admin\WilayaController@wilaya_delete');
 
 Route::get('/hub', 'admin\HubsController@hub_data');
 Route::get('/huball', 'admin\HubsController@hub_all');
-//Route::post('/addhub', 'admin\HubsController@hub_add');
+Route::post('/addhub', 'admin\HubsController@hub_add');
 Route::get('/showhub/{id}', 'admin\HubsController@hub_show');
 Route::patch('/edithub/{id}', 'admin\HubsController@hub_edit');
 Route::delete('/deletehub/{id}', 'admin\HubsController@hub_delete');
@@ -70,12 +70,21 @@ Route::get('/getuserdelete/{id}', 'admin\UsersController@show_user_delete');
 
 Route::patch('/editpassuser', 'admin\UsersController@edit_user_pass');
 Route::post('/addcom', 'admin\CommandesController@add_com');
+Route::post('/addcomwms', 'admin\CommandesController@add_com_wms');
+
+Route::get('/gotcommuns/{id}', 'admin\WilayaController@get_comuns');
 
 
 //colis 
 
 Route::get('/delivliste/{idliv}', 'admin\OnDeliveryController@data_colis_froute');
 Route::get('/getcolis/{id}', 'admin\ColisController@data_colis');
+Route::get('/getwmscolis/{id}', 'admin\ColisController@data_colis_wms');
+
+Route::get('/getmeproducts/{id}', 'admin\ColisController@data_colis_products');
+
+
+
 
 Route::get('/inhouse/{id}', 'admin\ColisController@data_colis_inhouse');
 
@@ -127,8 +136,14 @@ Route::get('/livreurfiche/{id}', 'admin\OnDeliveryController@detail_livreur_fich
 
 Route::get('/livreurstats/{id}', 'admin\OnDeliveryController@detail_livreur_stats');
 
+//----deliver update :::::::: ----------
+
+Route::post('/delivretour', 'admin\LivraisonController@deliver_retour');
 
 Route::get('/getstats', 'admin\StatsController@stats_tags');
+Route::get('/getdeliver/{id}', 'admin\StatsController@stats_deliver');
+
+
 
 Route::get('/getstatsclient', 'admin\StatsController@stats_tags_client');
 
@@ -153,11 +168,16 @@ Route::get('/tracksearch/{id}', 'admin\StatsColisController@tracking_show_search
 
 
 Route::get('/getcolisvaldiation/{id}', 'admin\ColisController@data_colis_validation');
+Route::get('/getcolisvaldiationwms/{id}', 'admin\ColisController@data_colis_validation_wms');
 
 
 Route::get('/getcolisrecp/{id}', 'admin\ColisController@data_colis_recp');
 
 Route::patch('/valdiationgrp', 'admin\ColisController@validate_colis_gp');
+
+Route::patch('/valdiationwmsgrp', 'admin\ColisController@validate_colis_gp_wms');
+
+
 Route::patch('/signalgrp', 'admin\ColisController@signaler_colis_gp');
 
 
@@ -235,6 +255,8 @@ Route::get('/deliv/{id}', 'admin\LivraisonController@get_delivred_livreur');
 
 Route::get('/codedeliv/{id}', 'admin\LivraisonController@code_delivred');
 
+Route::patch('/livred/{id}', 'admin\LivraisonController@delivred_qrcode');
+
 
 
 Route::get('/afterdelivred/{part}/{etat}/{wil}', 'admin\ConfirmedController@get_delivred_confirmed');
@@ -271,6 +293,7 @@ Route::patch('/cashbygp', 'admin\ColisController@update_all_livre');
 
 
 Route::get('/getcoms/{id}', 'admin\CommandesController@data_coms');
+Route::get('/getcomswms/{id}', 'admin\CommandesController@data_coms_wms');
 Route::get('/getarchivecoms/{id}', 'admin\CommandesController@data_coms_archive');
 
 Route::get('/getcolisinfos/{id}', 'admin\ColisController@infos_colis');
@@ -289,6 +312,9 @@ Route::post('/addcolis', 'admin\ColisController@add_colis');
 //-----------------------------------
 
 Route::patch('/tmpcolisv', 'admin\ColisController@tmp_colis_validate');
+Route::patch('/wmstmpcolisv', 'admin\ColisController@tmp_colis_validate_wms');
+
+
 
 Route::patch('/tmpsignalergrp', 'admin\ColisController@tmp_colis_signaler');
 
@@ -318,6 +344,10 @@ Route::get('/filtreflist/{id}', 'admin\OnDeliveryController@data_filtre_listfich
 Route::delete('/deletecolis/{id}', 'admin\ColisController@colis_delete');
 
 Route::get('/getmanifs/{id}', 'admin\CommandesController@data_manifs');
+
+Route::get('/getwmsmanifs/{id}', 'admin\CommandesController@data_manifs_wms');
+
+
 Route::get('/getarchivemanifs', 'admin\CommandesController@data_manifs_archive');
 
 
@@ -362,6 +392,7 @@ Route::get('/enlivstats/{idliv}', 'admin\OnDeliveryController@stats_liv_fiche');
 Route::patch('/validatemanif/{id}', 'admin\CommandesController@validate_manif');
 Route::patch('/cloturemanif/{id}', 'admin\CommandesController@cloture_manif');
 Route::patch('/validatemanifuser/{id}', 'admin\CommandesController@validate_manif_user');
+Route::patch('/validatewmsmanifuser', 'admin\CommandesController@validate_manif_wms');
 
 
 
@@ -395,5 +426,222 @@ Route::get('/bordereau', 'admin\BordereauController@data_bord');
 Route::post('/updatbord','admin\BordereauController@update_bord');
 
 
- // });
+//add products --------
+
+
+//Route::get('/addproduct', 'admin\BordereauController@add_product');
+
+Route::post('/addproduct', 'admin\wms\ProductsController@add_product');
+
+Route::get('/getstores/{idstore}', 'admin\wms\StoresController@get_stores');
+
+Route::get('/getproducts/{iduser}', 'admin\wms\ProductsController@list_product');
+
+Route::get('/wmstores', 'admin\wms\StoresController@list_partenaire');
+Route::get('/wmsallstores', 'admin\wms\StoresController@list_stores_wms');
+Route::get('/allstores', 'admin\wms\StoresController@liste_stores');
+Route::post('/addwmstore', 'admin\wms\StoresController@add_storewms');
+
+Route::get('/getwmslist/{store}', 'admin\wms\WmsController@list_wms_bystore');
+
+
+Route::get('/getstore/{id}', 'admin\wms\StoresController@brand_store_show');
+
+
+Route::post('/updtmystore/{id}', 'admin\wms\StoresController@brand_store');
+
+Route::get('/mystores/{id}', 'admin\wms\StoresController@my_stores');
+
+Route::post('/addpackbyuser', 'admin\wms\PickupController@add_package_user');
+
+
+// wms ----------- section --------------  : 
+
+
+Route::get('/getwms', 'admin\wms\WmsController@list_wms');
+
+Route::get('/getpickups/{id}', 'admin\wms\PickupController@list_pickups');
+
+Route::post('/pickqrcode', 'admin\wms\PickupController@approved_product');
+
+Route::get('/getpackages/{id}', 'admin\wms\PickupController@packages');
+Route::get('/getpackclosed/{id}', 'admin\wms\PickupController@packages_closed');
+Route::get('/packagesbystore/{id}', 'admin\wms\PickupController@packages_bystore');
+
+Route::get('/packagesbystore/{id}', 'admin\wms\PickupController@packages_bystore');
+
+Route::get('/packagesbyuser/{id}', 'admin\wms\PickupController@packages_bystore_user');
+
+Route::post('/addstoreuser', 'admin\wms\StoresController@add_store_user');
+Route::get('/countwms/{id}', 'admin\wms\StoresController@count_wms');
+
+
+Route::get('/getproducts', 'admin\wms\ProductsController@all_product');
+
+Route::get('/productstore/{id}', 'admin\wms\ProductsController@products_bystore_admin');
+
+Route::get('/pbystore/{id}', 'admin\wms\ProductsController@products_bystore_user');
+
+Route::get('/userproducts/{id}', 'admin\wms\ProductsController@products_bystore');
+
+
+Route::get('/wmstickets/{id}', 'admin\wms\PickupController@products_ticket');
+
+
+
+
+//pbystore
+
+Route::post('/addwms', 'admin\wms\WmsController@add_wms');
+Route::patch('/editwms/{idwms}', 'admin\wms\WmsController@edit_wms');
+
+
+Route::post('/addpackage', 'admin\wms\PickupController@add_package');
+Route::post('/addexistproduct', 'admin\wms\PickupController@add_exist_product');
+
+Route::patch('/failedpickup', 'admin\wms\PickupController@pickup_failed');
+
+Route::patch('/confirmticket', 'admin\wms\PickupController@pickup_close');
+
+
+Route::patch('/validpickup/{id}', 'admin\wms\PickupController@validation_pickup_user');
+Route::patch('/delpickups/{id}', 'admin\wms\PickupController@delete_pickup_user');
+
+Route::post('/delfieldpickup/{id}', 'admin\wms\PickupController@delete_fpickup_user');
+Route::patch('/editfieldpickup/{id}', 'admin\wms\PickupController@edit_fpickup_user');
+
+
+
+//------------- edit colis ----------------
+Route::patch('/fixwilaya', 'admin\ColisController@fix_colis_wilaya');
+Route::patch('/fixcommun', 'admin\ColisController@fix_colis_commune');
+
+Route::patch('/fixproduct', 'admin\ColisController@fix_colis_product');
+
+
+
+
+
+
+
+//------------------------ gets tags wilaya 
+
+
+
+Route::get('/getwilas', 'admin\WilayaController@get_wilaya_arr');
+Route::get('/getcommuns', 'admin\WilayaController@get_communs_arr');
+
+Route::get('/filtrcomuns/{id}', 'admin\WilayaController@get_communs_filtre');
+
+
+
+
+//---------- WMS ----- Transit ---------------
+
+
+Route::get('/wmsendtrasnit/{id}', 'admin\wms\TransitController@index_send');
+Route::get('/wmsrecivtrasnit/{id}', 'admin\wms\TransitController@index_receiver');
+
+Route::get('/getwmstransinfo/{id}','admin\wms\TransitController@my_wmshub');
+Route::post('/addwmstransit', 'admin\wms\TransitController@add_transit_wms');
+
+Route::get('/wmshubs', 'admin\wms\TransitController@list_wms');
+
+// details send wsm transit -------
+
+Route::get('/inwmsdata/{id}', 'admin\wms\TransitController@get_wms_products');
+Route::get('/getwsmtransitp/{id}', 'admin\wms\TransitController@transit_wms_products');
+
+Route::post('/delwmstrans', 'admin\wms\TransitController@transit_send_product_remove');
+
+Route::post('/addproducttrans', 'admin\wms\TransitController@transit_send_product_add');
+
+Route::patch('/cloturetransit', 'admin\wms\TransitController@transit_send_cloture');
+Route::get('/productinfo/{id}/{transit}', 'admin\wms\TransitController@product_info');
+Route::patch('/cancelproduct', 'admin\wms\TransitController@cancel_product_transit');
+
+
+Route::get('/getmanagerproducts/{id}', 'admin\wms\ProductsController@products_manager');
+Route::get('/managerstores/{id}', 'admin\wms\StoresController@stores_manager');
+Route::get('/managerpackages/{id}', 'admin\wms\PickupController@pickups_manager');
+
+Route::patch('/confirmproductransit', 'admin\wms\TransitController@confirm_product_transit');
+
+
+Route::patch('/transitcloture', 'admin\wms\TransitController@transit_receive_cloture');
+
+//---------- Hub Wilaya  -----  la liste des wilayas  ---------------
+
+Route::get('/wilayabyhub/{id}', 'admin\HubsController@list_wilayabyhubs');
+
+Route::post('/addwiltohub', 'admin\HubsController@add_wilaya_tohub');
+Route::patch('/delwiltohub', 'admin\HubsController@delete_wilaya_tohub');
+
+
+//------- zonnes ------------ listing routes 
+
+
+Route::get('/zones', 'admin\ZonesController@list_zones');
+Route::post('/addzone', 'admin\ZonesController@add_zone');
+Route::patch('/editzone', 'admin\ZonesController@edit_zone');
+Route::post('/delzone', 'admin\ZonesController@delete_zone');
+
+
+Route::get('/communsbyzone/{id}', 'admin\ZonesController@list_zone_communes');
+Route::get('/getwalls/{id}', 'admin\ZonesController@list_wilaya');
+Route::get('/zonegetcomuns/{id}/{wilaya}', 'admin\ZonesController@list_communs');
+Route::post('/addcomszone', 'admin\ZonesController@add_zone_commun');
+Route::post('/delcomzone', 'admin\ZonesController@delete_zone_commun');
+
+
+Route::get('/listdeliverszone/{id}', 'admin\ZonesController@list_zone_delivers');
+Route::get('/getzdelivers/{id}', 'admin\ZonesController@zone_delivers');
+Route::post('/deliveraddzone', 'admin\ZonesController@add_zone_deliver');
+Route::post('/delzondeliver', 'admin\ZonesController@delete_zone_deliver');
+Route::get('/gotdeliverz/{id}', 'admin\ZonesController@show_deliver');
+Route::patch('/delivereditzone', 'admin\ZonesController@edit_deliver');
+
+Route::patch('/enabldeliver/{id}', 'admin\ZonesController@enable_deliver');
+Route::patch('/disabldeliver/{id}', 'admin\ZonesController@disable_deliver');
+
+
+//---------- dispatch :::: 
+
+Route::post('/deliverdispatch/{id}/{added}', 'admin\ZonesController@dispatch_deliver');
+Route::get('/ondeliverynow/{id}', 'admin\inventory\InventoryController@list_delivers');
+Route::get('/ondelivnow/{id}', 'admin\inventory\InventoryController@on_delivery_list');
+
+
+Route::get('/getAllSheets/{q?}', 'api\GoogleSheetApiController@getAllSheets')->name('getAllSheets');
+
+
+//------- Google API Sheets ------------
+
+/**
+ * Adding URL Of Sheet
+ */
+Route::post('/addSheet', 'api\GoogleSheetApiController@addSheet');
+Route::get('/editSheet/{id}', 'api\GoogleSheetApiController@editSheet');
+Route::patch('/editSheet/{id}', 'api\GoogleSheetApiController@updateSheet');
+Route::delete('/deleteSheet/{id}', 'api\GoogleSheetApiController@deleteSheet');
+
+/**
+ * Add Tab Sheet Name
+ */
+Route::get('/addTabSheet/{id}', 'api\GoogleSheetApiController@addTabSheet')->name('addTabSheet');
+Route::post('/addTabSheet/{id}', 'api\GoogleSheetApiController@storeTabSheet')->name('storeTabSheet');
+Route::delete('/deleteTab/{id}', 'api\GoogleSheetApiController@deleteTab');
+
+/**
+ * Get Tabs
+ */
+Route::get('/getAllTabs/{id}', 'api\GoogleSheetApiController@getAllTabs');
+/**
+ * Getting Data from Google Sheets
+ */
+Route::get('/getSheet', 'api\GoogleSheetApiController@getSheet')->name('getSheetData');
+
+
+
+ //});
 
